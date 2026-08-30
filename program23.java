6de536b6a7cf1eaeb23351a7c4716cc61cc19662
Ayush Rana <ayushhrana07@gmail.com>
2026-08-30 22:18:39 +0530

    Add temperature conversion program in Java
    Implement temperature conversion from Celsius to Fahrenheit.

---
+++ program23.java
@@ -0,0 +1,17 @@
+// Question 23. Input a temperature in Celsius and convert it to Fahrenheit using type casting and expressions.
+
+import java.util.Scanner;
+
+public class Program23 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = sc.nextDouble();
+
+        double fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+        System.out.println("Temperature in Fahrenheit = " + fahrenheit);
+    }
+}
